cf157b02ead15969bb560b5eaf5e09e94b6a8305
Bryan Penuntun <cyberjrs.p@gmail.com>
2024-09-20 11:19:59 +0700

    buat API JSON

--- index.js
+++ index.js
@@ -4,8 +4,34 @@ const http = require('http');
 // buat server
 const server = http.createServer((req, res) =>
 {
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.write('Selamat Pagi');
+    // res.writeHead(200,{'Content-Type': 'text/html'});
+    // res.write('Selamat Pagi');
+    
+    
+    res.writeHead(200,{'Content-Type': 'application/json'});
+
+    if(req.url === '/book'){
+        message = 'List Buku';
+        data = ['Harry Portter','Garvil'];
+    }else if (req.url === '/movie'){
+        message = 'List Movie';
+        data = ['Cars','UP'];
+    }else if (req.url === '/fakultas'){
+        message = 'List Fakultas';
+        data = ['Ekonomi dan bisnis','Ilmu Komputer Rekayasa'];
+    }else if (req.url === '/prodi'){
+        message = 'List Prodi';
+        data = ['Sistem Informasi','Teknologi Informasi'];
+    } else{
+        message = 'Tidak Ditemukan';
+        data = [];
+    }
+
+    res.write(JSON.stringify({
+        'message': message,
+        'data':data,
+        'status': 'success'
+    }));
     res.end();
 });
 
